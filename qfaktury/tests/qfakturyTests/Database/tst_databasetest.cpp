5a2c9931ffd71b9a215502e3105a410f48603ae0
prog <tdk81@wp.pl>
2013-01-02 13:40:51 +0100

    - Corrected .gitignore files so that we don't VCS garbages

--- qfaktury/tests/qfakturyTests/Database/tst_databasetest.cpp
+++ qfaktury/tests/qfakturyTests/Database/tst_databasetest.cpp
@@ -1,6 +1,9 @@
 #include <QtCore/QString>
 #include <QtTest/QtTest>
 
+#include "Database.h"
+
+
 class DatabaseTest : public QObject
 {
     Q_OBJECT
@@ -11,8 +14,30 @@ public:
 private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
-    void testCase1();
-    void testCase1_data();
+
+    void testCaseCheckDBFileName();
+
+    void testCaseCheckModelCommodity();
+    void testCaseCheckModelCommodityType();
+    void testCaseCheckModelCounterparty();
+    void testCaseCheckModelCounterpartyType();
+    void testCaseCheckModelCountry();
+    void testCaseCheckModelCurrency();
+    void testCaseCheckModelEmail();
+    void testCaseCheckModelInvoice();
+    void testCaseCheckModelInvoiceType();
+    void testCaseCheckModelInvoiceWithCommodities();
+    void testCaseCheckModelPaymentType();
+    void testCaseCheckModelPhone();
+    void testCaseCheckModelUnit();
+    void testCaseCheckModelVat();
+
+    void testCaseCheckCommodities();
+    void testCaseCheckInvoiceWithCommoditiesInsertTransact();
+    void testCaseCheckInvoiceWithCommoditiesDeleteTransact();
+
+//    void testCase1();
+//    void testCase1_data();
 };
 
 DatabaseTest::DatabaseTest()
@@ -21,23 +46,157 @@ DatabaseTest::DatabaseTest()
 
 void DatabaseTest::initTestCase()
 {
+    QCoreApplication::setApplicationName("QFaktury");
+    QCoreApplication::setOrganizationName("www.e-linux.pl");
+    QCoreApplication::setOrganizationDomain("www.e-linux.pl");
+    QCoreApplication::setApplicationVersion(APP_VERSION);
 }
 
 void DatabaseTest::cleanupTestCase()
 {
 }
 
-void DatabaseTest::testCase1()
+
+
+void DatabaseTest::testCaseCheckDBFileName()
 {
-    QFETCH(QString, data);
-    QVERIFY2(true, "Failure");
+    Database db;
+    const QString filename(QString("%1-%2.db3").arg(QCoreApplication::applicationName()).arg(APP_VERSION));
+    QCOMPARE(db.dbFileName(), filename);
 }
 
-void DatabaseTest::testCase1_data()
+
+void DatabaseTest::testCaseCheckModelCommodity()
 {
-    QTest::addColumn<QString>("data");
-    QTest::newRow("0") << QString();
+    Database db;
+    QVERIFY(db.modelCommodity() != NULL);
 }
+
+
+void DatabaseTest::testCaseCheckModelCommodityType()
+{
+    Database db;
+    QVERIFY(db.modelCommodityType() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelCounterparty()
+{
+    Database db;
+    QVERIFY(db.modelCounterparty()!= NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelCounterpartyType()
+{
+    Database db;
+    QVERIFY(db.modelCounterpartyType() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelCountry()
+{
+    Database db;
+    QVERIFY(db.modelCountry() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelCurrency()
+{
+    Database db;
+    QVERIFY(db.modelCurrency() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelEmail()
+{
+    Database db;
+    QVERIFY(db.modelEmail() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelInvoice()
+{
+    Database db;
+    QVERIFY(db.modelInvoice() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelInvoiceType()
+{
+    Database db;
+    QVERIFY(db.modelInvoiceType() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelInvoiceWithCommodities()
+{
+    Database db;
+    QVERIFY(db.modelInvoiceWithCommodities() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelPaymentType()
+{
+    Database db;
+    QVERIFY(db.modelPaymentType() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelPhone()
+{
+    Database db;
+    QVERIFY(db.modelPhone() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelUnit()
+{
+    Database db;
+    QVERIFY(db.modelUnit() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckModelVat()
+{
+    Database db;
+    QVERIFY(db.modelVat() != NULL);
+}
+
+
+void DatabaseTest::testCaseCheckCommodities()
+{
+    Database db;
+    QVERIFY(db.commodities(0).size() == 0);
+}
+
+
+void DatabaseTest::testCaseCheckInvoiceWithCommoditiesInsertTransact()
+{
+    Database db;
+    InvoiceData invData;
+    QList<CommodityVisualData> commodities;
+
+    //db.invoiceWithCommoditiesInsertTransact(invData, commodities);
+}
+
+
+void DatabaseTest::testCaseCheckInvoiceWithCommoditiesDeleteTransact()
+{
+    Database db;
+}
+
+//void DatabaseTest::testCase1()
+//{
+//    QFETCH(QString, data);
+//    QVERIFY2(true, "Failure");
+//}
+
+//void DatabaseTest::testCase1_data()
+//{
+//    QTest::addColumn<QString>("data");
+//    QTest::newRow("0") << QString();
+//}
 
 QTEST_APPLESS_MAIN(DatabaseTest)
 
