2ebeb7cb521ff4051078d4e19a95a0a31142198d
prog <tdk81@wp.pl>
2013-01-06 21:36:38 +0100

    - added tests of CountryData.h

--- qfaktury/tests/qfakturyTests/CountryData/tst_countrydatatest.cpp
+++ qfaktury/tests/qfakturyTests/CountryData/tst_countrydatatest.cpp
@@ -1,6 +1,8 @@
 #include <QtCore/QString>
 #include <QtTest/QtTest>
 
+#include "Database.h"
+
 class CountryDataTest : public QObject
 {
     Q_OBJECT
@@ -11,8 +13,8 @@ public:
 private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
-    void testCase1();
-    void testCase1_data();
+    void testCaseCheckDBFields();
+    void testCaseCheckDBFields_data();
 };
 
 CountryDataTest::CountryDataTest()
@@ -21,22 +23,43 @@ CountryDataTest::CountryDataTest()
 
 void CountryDataTest::initTestCase()
 {
+    QCoreApplication::setApplicationName("QFaktury");
+    QCoreApplication::setOrganizationName("www.e-linux.pl");
+    QCoreApplication::setOrganizationDomain("www.e-linux.pl");
+    QCoreApplication::setApplicationVersion(APP_VERSION);
+
+    const QString dbFilename(QString("%1-%2.db3").arg(QCoreApplication::applicationName()).arg(APP_VERSION));
+    if(QFile::exists(dbFilename))
+    {
+        QDir dir;
+        dir.remove(dbFilename);
+    }
 }
 
 void CountryDataTest::cleanupTestCase()
 {
 }
 
-void CountryDataTest::testCase1()
+void CountryDataTest::testCaseCheckDBFields()
 {
-    QFETCH(QString, data);
-    QVERIFY2(true, "Failure");
+    QFETCH(QString, field_name);
+    QFETCH(int, field_num);
+
+    Database db;
+    QSqlQuery query(db.modelCountry()->query());
+
+    QVERIFY2(query.exec(QString("SELECT %1 FROM 'country'").arg(field_name)),
+             QString("Missing DB field in the table 'country': %1").arg(field_name).toAscii());
+    QCOMPARE(db.modelCountry()->fieldIndex(field_name), field_num);
 }
 
-void CountryDataTest::testCase1_data()
+void CountryDataTest::testCaseCheckDBFields_data()
 {
-    QTest::addColumn<QString>("data");
-    QTest::newRow("0") << QString();
+    QTest::addColumn<QString>("field_name");
+    QTest::addColumn<int>("field_num");
+
+    QTest::newRow("id_country") << QString("id_country") << (int)CountryFields::ID_COUNTRY;
+    QTest::newRow("name") << QString("name") << (int)CountryFields::NAME;
 }
 
 QTEST_APPLESS_MAIN(CountryDataTest)
